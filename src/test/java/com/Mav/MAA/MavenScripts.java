c7da4c727bdfb612424fc8b2f9a84b1d487816e8
jalda123 <sateesh.jalda2020@gmail.com>
2018-10-02 11:28:10 +0530

    second commit

--- src/test/java/com/Mav/MAA/MavenScripts.java
+++ src/test/java/com/Mav/MAA/MavenScripts.java
@@ -2,9 +2,34 @@ package com.Mav.MAA;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class MavenScripts { 
+	ExtentHtmlReporter report;
+	
+	ExtentReports extent;
+	
+	ExtentTest logger;
+	
+	@BeforeTest
+	
+	public void extent() {
+		
+		 report = new ExtentHtmlReporter("./Reports/learn-automation.html");
+		
+		 extent = new ExtentReports();
+		
+		extent.attachReporter(report);
+		
+		 logger = extent.createTest("LoginTest");
+	}
 	
 	@Test
 	
@@ -15,6 +40,21 @@ public class MavenScripts {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.softwaretestingmaterial.com/selenium-interview-questions/");
+		
+		logger.log(Status.INFO, "Login to Google Application");
+		
+		logger.log(Status.PASS, "Test case is Passed");
+		
+		
+	}
+	
+	
+	
+	@AfterTest()
+	
+	public void teardown() {
+		
+		extent.flush();
 	}
 
 }
